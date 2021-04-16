3d7260cea96605aa44b84d38ae5798ea01d9edd5
Manoj Kumar G <839478@cognizant.com>
2021-04-16 19:42:06 +0530

    initial cloudapp structure

---
+++ cloud-app/src/main/java/com/cloudapp/controller/DashboardController.java
@@ -0,0 +1,14 @@
+package com.cloudapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DashboardController {
+
+	@RequestMapping(value = "/dashboard")
+	public String loadDashboard() {
+		return "index";
+	}
+
+}
