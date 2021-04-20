67c38e9c07e39ccf8c350311cd9959d4c520263f
Priyadharshini <842105@cognizant.com>
2021-04-20 15:03:53 +0530

    Priyadharshini-842105 week5 assignment submission

---
+++ cloud-app/src/main/java/com/cloudapp/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.cloudapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cloudapp.vo.Student;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+	@RequestMapping("/showForm")
+	public String showForm(Model model) {
+		// create a student object
+		Student theStudent=new Student();
+		// add student object to the model
+		model.addAttribute("student",theStudent);
+		 // add the country options to the model 
+	
+		return "student-form";
+	}
+	@RequestMapping("/processForm")
+	public String processForm(@ModelAttribute("student") Student theStudent) {
+		// log the input data
+		System.out.println("theStudent: "+theStudent.getFirstName()+" "+theStudent.getLastName());
+		return "student-confirmation";
+	}
+}
---
+++ cloud-app/src/main/webapp/WEB-INF/jsp/main-menu.jsp
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h2>Spring MVC Demo - Home page</h2>
+
+<hr>
+<a href ="hello/showForm"><button>Hello World form</button></a>
+
+<br><br>
+
+<a href ="student/showForm"><button>student form</button></a>
+
+<br><br>
+
+<a href ="customer/showForm"><button>customer form</button></a>
+</body>
+
+</html>
