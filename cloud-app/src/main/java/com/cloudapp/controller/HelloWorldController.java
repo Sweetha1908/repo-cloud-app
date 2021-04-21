7563eea9dd2f26ccca2066a5e16bf2bc20b27751
Siva <SivaSuryaReddy.Medapativ@cognizant.com>
2021-04-21 10:03:43 +0530

    Siva week-5 assignment

---
+++ cloud-app/src/main/java/com/cloudapp/controller/HelloWorldController.java
@@ -0,0 +1,48 @@
+package com.cloudapp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+	// controller method to show the initial HTML form
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	// controller method to process the HTML form
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	// controller method to read form data and
+		// add data to the model
+
+	@RequestMapping("/processFormTwo")
+	public String anotherMethod(HttpServletRequest request,Model model) {
+		// read the request parameter from the HTML form
+		String theName=request.getParameter("studentName");
+		// convert the data to all caps
+		theName=theName.toUpperCase();
+		// create the message
+		String result="Hi! "+theName;
+		// add message to the model
+		model.addAttribute("message", result);
+		return "helloworld";
+	}
+	@RequestMapping("/processFormThree")
+	public String anotherParamMethod(@RequestParam("studentName") String theName,Model model) {
+		// convert the data to all caps
+		theName=theName.toUpperCase();
+		// create the message
+		String result="Hi! "+theName;
+		// add message to the model
+		model.addAttribute("message", result);
+		return "helloworld";
+	}
+
+}
